08bbf11cfd8cc133110bb94e2cb1a2fe97e99156
Vinh Vu <vinhhv@uci.edu>
2023-11-21 12:25:01 -0800

    First set of tests

---
+++ src/test/scala/com/rockthejvm/jobsboard/fixtures/JobFixture.scala
@@ -0,0 +1,107 @@
+package com.rockthejvm.jobsboard.fixtures
+
+import cats.syntax.all.*
+import com.rockthejvm.jobsboard.domain.job.*
+
+import java.util.UUID
+
+trait JobFixture {
+
+  val NotFoundJobUuid = UUID.fromString("6ea79557-3112-4c84-a8f5-1d1e2c300948")
+
+  val AwesomeJobUuid = UUID.fromString("843df718-ec6e-4d49-9289-f799c0f40064")
+
+  val AwesomeJob = Job(
+    AwesomeJobUuid,
+    1659186086L,
+    "daniel@rockthejvm.com",
+    JobInfo(
+      "Awesome Company",
+      "Tech Lead",
+      "An awesome job in Berlin",
+      "https://rockthejvm.com/awesomejob",
+      false,
+      "Berlin",
+      2000.some,
+      3000.some,
+      "EUR".some,
+      "Germany".some,
+      Some(List("scala", "scala-3", "cats")),
+      None,
+      "Senior".some,
+      None
+    )
+  )
+
+  val InvalidJob = Job(
+    null,
+    42L,
+    "nothing@gmail.com",
+    JobInfo.empty
+  )
+
+  val UpdatedAwesomeJob = Job(
+    AwesomeJobUuid,
+    1659186086L,
+    "daniel@rockthejvm.com",
+    JobInfo(
+      "Awesome Company (Spain Branch)",
+      "Engineering Manager",
+      "An awesome job in Barcelona",
+      "http://www.awesome.com",
+      false,
+      "Barcelona",
+      2200.some,
+      3200.some,
+      "USD".some,
+      "Spain".some,
+      Some(List("scala", "scala-3", "zio")),
+      "http://www.awesome.com/logo.png".some,
+      "Highest".some,
+      "Some additional info".some
+    )
+  )
+
+  val RockTheJvmNewJob = JobInfo(
+    "RockTheJvm",
+    "Technical Author",
+    "For the glory of the RockTheJvm!",
+    "https://rockthejvm.com/",
+    true,
+    "From remote",
+    2000.some,
+    3500.some,
+    "EUR".some,
+    "Romania".some,
+    Some(List("scala", "scala-3", "cats", "akka", "spark", "flink", "zio")),
+    None,
+    "High".some,
+    None
+  )
+
+  val RockTheJvmJobWithNotFoundId = AwesomeJob.copy(id = NotFoundJobUuid)
+
+  val AnotherAwesomeJobUuid = UUID.fromString("19a941d0-aa19-477b-9ab0-a7033ae65c2b")
+  val AnotherAwesomeJob     = AwesomeJob.copy(id = AnotherAwesomeJobUuid)
+
+  val RockTheJvmAwesomeJob =
+    AwesomeJob.copy(jobInfo = AwesomeJob.jobInfo.copy(company = "RockTheJvm"))
+
+  val NewJobUuid = UUID.fromString("efcd2a64-4463-453a-ada8-b1bae1db4377")
+  val AwesomeNewJob = JobInfo(
+    "Awesome Company",
+    "Tech Lead",
+    "An awesome job in Berlin",
+    "example.com",
+    false,
+    "Berlin",
+    2000.some,
+    3000.some,
+    "EUR".some,
+    "Germany".some,
+    Some(List("scala", "scala-3", "cats")),
+    None,
+    "High".some,
+    None
+  )
+}
---
+++ src/test/scala/com/rockthejvm/jobsboard/http/routes/JobRoutesSpec.scala
@@ -0,0 +1,130 @@
+package com.rockthejvm.jobsboard.http.routes
+
+import cats.effect.*
+import cats.effect.testing.scalatest.AsyncIOSpec
+import cats.implicits.*
+import com.rockthejvm.jobsboard.core.*
+import com.rockthejvm.jobsboard.domain.job.*
+import com.rockthejvm.jobsboard.fixtures.*
+import io.circe.generic.auto.*
+import org.http4s.*
+import org.http4s.Uri.*
+import org.http4s.circe.CirceEntityCodec.*
+import org.http4s.dsl.*
+import org.http4s.implicits.*
+import org.scalatest.freespec.AsyncFreeSpec
+import org.scalatest.matchers.should.Matchers
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+
+import java.util.UUID
+
+class JobRoutesSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers with Http4sDsl[IO] with JobFixture {
+  //////////////////////////////////////////////////////////////////////////////////////////////
+  /// prep
+  //////////////////////////////////////////////////////////////////////////////////////////////
+  val jobs: Jobs[IO] = new Jobs[IO] {
+    override def create(ownerEmail: String, jobInfo: JobInfo): IO[UUID] =
+      IO.pure(NewJobUuid)
+
+    override def all(): IO[List[Job]] =
+      IO.pure(List(AwesomeJob))
+
+    override def find(id: UUID): IO[Option[Job]] =
+      if (id == AwesomeJobUuid) IO.pure(Some(AwesomeJob))
+      else IO.pure(None)
+
+    override def update(id: UUID, jobInfo: JobInfo): IO[Option[Job]] =
+      if (id == AwesomeJobUuid) IO.pure(Some(AwesomeJob))
+      else IO.pure(None)
+
+    override def delete(id: UUID): IO[Int] =
+      if (id == AwesomeJobUuid) IO.pure(1)
+      else IO.pure(0)
+  }
+
+  given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+  // this is what we are testing
+  val jobRoutes: HttpRoutes[IO] = JobRoutes[IO](jobs).routes
+
+  //////////////////////////////////////////////////////////////////////////////////////////////
+  /// tests
+  //////////////////////////////////////////////////////////////////////////////////////////////
+
+  "JobRoutes" - {
+    "should return a job with a given id" in {
+      val uri       = uri"/jobs"
+      val uriWithId = uri / AwesomeJobUuid.toString
+      for {
+        response <- jobRoutes.orNotFound.run(
+          Request(method = Method.GET, uri = uriWithId)
+        )
+        retrieved <- response.as[Job]
+      } yield {
+        response.status shouldBe Status.Ok
+        retrieved shouldBe AwesomeJob
+      }
+    }
+
+    "should return all jobs" in {
+      for {
+        response <- jobRoutes.orNotFound.run(
+          Request(method = Method.POST, uri = uri"/jobs")
+        )
+        retrieved <- response.as[List[Job]]
+      } yield {
+        response.status shouldBe Status.Ok
+        retrieved shouldBe List(AwesomeJob)
+      }
+    }
+
+    "should create a new job" in {
+      for {
+        response <- jobRoutes.orNotFound.run(
+          Request(method = Method.POST, uri = uri"/jobs/create")
+            .withEntity(AwesomeJob.jobInfo)
+        )
+        retrieved <- response.as[UUID]
+      } yield {
+        response.status shouldBe Status.Created
+        retrieved shouldBe NewJobUuid
+      }
+    }
+
+    "should only update a job that exists" in {
+      val uri              = uri"/jobs"
+      val uriWithId        = uri / AwesomeJobUuid.toString
+      val uriWithInvalidId = uri / NotFoundJobUuid.toString
+      for {
+        responseOk <- jobRoutes.orNotFound.run(
+          Request(method = Method.PUT, uri = uriWithId)
+            .withEntity(UpdatedAwesomeJob.jobInfo)
+        )
+        responseInvalid <- jobRoutes.orNotFound.run(
+          Request(method = Method.PUT, uri = uriWithInvalidId)
+            .withEntity(UpdatedAwesomeJob.jobInfo)
+        )
+      } yield {
+        responseOk.status shouldBe Status.Ok
+        responseInvalid.status shouldBe Status.NotFound
+      }
+    }
+
+    "should only delete a job that exists" in {
+      val uri              = uri"/jobs"
+      val uriWithId        = uri / AwesomeJobUuid.toString
+      val uriWithInvalidId = uri / NotFoundJobUuid.toString
+      for {
+        responseOk <- jobRoutes.orNotFound.run(
+          Request(method = Method.DELETE, uri = uriWithId)
+        )
+        responseInvalid <- jobRoutes.orNotFound.run(
+          Request(method = Method.DELETE, uri = uriWithInvalidId)
+        )
+      } yield {
+        responseOk.status shouldBe Status.Ok
+        responseInvalid.status shouldBe Status.NotFound
+      }
+    }
+  }
+}
